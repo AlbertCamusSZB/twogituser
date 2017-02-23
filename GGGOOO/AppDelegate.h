714222e1c80b620bbf9f44b78f37ad584f55234c
songzhibin <2881569168@qq.com>
2017-02-23 10:56:54 +0800

    ggg

---
+++ GGGOOO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GGGOOO
+//
+//  Created by chenjiu89-MacMin on 2017/2/23.
+//  Copyright © 2017年 camus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
